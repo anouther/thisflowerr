a4c2dcb5316d28e7a516d63f89e9349c859b6c84
anouther <155570107+anouther@users.noreply.github.com>
2024-10-16 21:05:37 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('GIPY, I LOVE YOU').split('');
+        const titles = ('Stay by my side').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Makasih udah mau percaya sama aku buat bikinin hadiah buat kalian kasih ke orang spesial 🤍🤍";
+            subtitle.innerText = " I want you to stay by my side, always there when i'm at my lowest point, i don't want to lose someone like you. You are very special, perfect, worthy of love and precious..don't say mean things to yourself, you are deserve many love. I am very grateful that you are by my side, i feel comfortable and safe with you. I'm a really lucky person to have you by my side.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
